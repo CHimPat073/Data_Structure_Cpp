56008bd7c21edb2873880f777b6b3da5b892a84c
CHimPat073 <himanshupathak073@gmail.com>
2025-07-21 23:10:13 +0530

    Did Palindrome Question on Leetcode

---
+++ Supreme/Questions/LC_Palindrome.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPalindrome(int x) {
+    long long revNum =0;
+    int dup=x;
+     while(x>0){
+        int id=x%10;
+        revNum=revNum*10 +id;
+        x/=10;   
+    }
+    if(dup == revNum) {
+        return true;
+    } else {
+        
+        return false;
+    }
+}
+int main(){
+
+    int res = isPalindrome(120);
+    cout<<res;
+    return 0;
+}
