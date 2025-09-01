d9cea53adf2d68e423d6d11ee718927076245adc
CHimPat073 <himanshupathak073@gmail.com>
2025-09-02 02:03:42 +0530

    Change code and also did it on GFG

--- Supreme/Questions/MaxMinNum.cpp
+++ Supreme/Questions/MaxMinNum.cpp
@@ -1,28 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-int MaxNUm(vector<int> arr){
-    int maxi=INT_MIN;
-    for(int i=0; i<arr.size(); i++){
-        if(arr[i]>maxi){
-            maxi=arr[i];
+pair<int, int> getMinMax(vector<int> arr) {
+        // code here
+        int min = INT_MAX;
+        int max= INT_MIN;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            if(arr[i]>max){
+                max=arr[i];
+            }
         }
+        return {min,max};
     }
-    return maxi;
-}
-
-int MinNUm(vector<int> arr){
-    int mini=INT_MAX;
-    for(int i=0; i<arr.size(); i++){
-        if(arr[i]<mini){
-            mini=arr[i];
-        }
-    }
-    return mini;
-}
 
 int main(){
     vector<int> arr = {10,2,35,4,55,6,-1};
-    cout<<"Maximum number is: "<<MaxNUm(arr)<<endl;
-    cout<<"Minimum number is: "<<MinNUm(arr)<<endl;
+    pair<int,int> ans =getMinMax(arr);
+    cout<<"Minimum Element is: "<<ans.first<<endl;
+    cout<<"Maximum Element is: "<<ans.second<<endl;
+
+    
     return 0;
 }
