8f4e577178faac0405e20759494df4ac9f482440
CHimPat073 <himanshupathak073@gmail.com>
2025-09-05 23:45:10 +0530

    Question

---
+++ Supreme/Loops/Patterns/RotateArrayby1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+void PrintArray(vector<int>& arr) {
+    for(auto i : arr) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+void rotateArraybyone(vector<int>& arr) {
+        int n= arr.size();
+        int temp =arr[0];
+        for(int i=0;i<n;i++){
+            arr[i-1]=arr[i];
+        }
+        arr[n-1]=temp;
+        PrintArray(arr);
+    }
+ int main(){
+    vector<int> arr={1, 2, 3, 4, 5};
+    rotateArraybyone(arr);
+    return 0;
+}
