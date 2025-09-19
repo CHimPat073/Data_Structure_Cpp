fc51581225a90b31f00487497028940b859bb14c
CHimPat073 <himanshupathak073@gmail.com>
2025-09-20 00:20:45 +0530

    Did Optimal Approch'

--- Supreme/Questions/AlternateArrange.cpp
+++ Supreme/Questions/AlternateArrange.cpp
@@ -20,11 +20,24 @@ for(int i=0;i<n;i++){
 return nums;
 }
 vector<int>reaarangeArrayOptimal(vector<int>& nums){
-    
+    int n= nums.size();
+    vector<int> ans(n,0);
+    int posIndex=0,negIndex=1;
+    for(int i=0;i<n;i++){
+        if(nums[i]<=0){
+            ans[negIndex]=nums[i];
+            negIndex+=2;
+        } else {
+            ans[posIndex]=nums[i];
+            posIndex+=2;
+        }
+    }
+    return ans;
 }
 int main(){
     vector<int> arr={3,1,-2,-5,2,-4};
     rearrangeArray(arr);
+    reaarangeArrayOptimal(arr);
     for(auto x:arr){
         cout<<x<<" ";
     }
