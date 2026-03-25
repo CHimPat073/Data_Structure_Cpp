9b02abd705be2723888eecd3076a9d3bb96526cb
CHimPat073 <himanshupathak073@gmail.com>
2026-03-25 15:42:14 +0530

    Reverse a LL using Loops

---
+++ Supreme/Linked List/Question/ReverseALL.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(){
+        this->data=0;
+        this->next=NULL;
+    }
+
+    Node (int val){
+        this->data=val;
+        this->next=NULL;
+    }
+     
+}; 
+void Print(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+Node* reverseList(Node* head) {
+    Node* prev=NULL;
+    Node* curr=head;
+
+    while(curr!=NULL){
+        Node* fwd=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=fwd;
+
+        
+    }
+    return prev;
+}
+
+int main(){
+    Node* head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    head->next->next->next=new Node(40);
+
+    cout<<"Before Reversing the List: "<<endl;
+    Print(head);
+
+    head=reverseList(head);
+    cout<<endl;
+    cout<<"After Reversing the List: "<<endl;
+    Print(head);
+
+    return 0;
+}
