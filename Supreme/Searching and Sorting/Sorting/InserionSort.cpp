8fd051f5a7bcee375bb25615128fed96762d40e9
CHimPat073 <himanshupathak073@gmail.com>
2026-07-06 00:24:30 +0530

    Sorting

--- Supreme/Searching and Sorting/Sorting/InserionSort.cpp
+++ Supreme/Searching and Sorting/Sorting/InserionSort.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 void insertionSort(vector<int>& arr){
     int n = arr. size();
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j]<arr[j-1]){
+            swap(arr[j],arr[j-1]);
+            j--;
+        }
+    }
 }
 int main(){
     vector<int> arr={64, 34, 25, 12, 22, 11, 90};
