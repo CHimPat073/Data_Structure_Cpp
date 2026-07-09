042e11f60370e6e7ef21285110cfab20c49a901c
CHimPat073 <himanshupathak073@gmail.com>
2026-07-09 17:26:27 +0530

    Update Supreme/Stack & Queue: VaildParenthesis

--- Supreme/Stack & Queue/VaildParenthesis.cpp
+++ Supreme/Stack & Queue/VaildParenthesis.cpp
@@ -1,7 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool isValid(string s) {
+        stack <char> st;
+        
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
 
+            //open bracket
+            if( ch =='(' || ch =='{'|| ch=='[' ){
+                st.push(ch);
+            }
+            else{
+                //closing
+                if(!st.empty()){
+                    char topCH=st.top();
+                    if(ch ==')' && topCH =='('){
+                        st.pop();
+                    }
+                    else if(ch =='}' && topCH =='{'){
+                        st.pop();
+                    }
+                    else if(ch==']' && topCH=='['){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 int main(){
-    cout<<"hello World!";
+    string s="()[]{]";
+    if(isValid(s)){
+        cout<<"Valid Parenthesis"<<endl;
+    }
+    else{
+        cout<<"Invalid Parenthesis"<<endl;
+    }
     return 0;
 }
