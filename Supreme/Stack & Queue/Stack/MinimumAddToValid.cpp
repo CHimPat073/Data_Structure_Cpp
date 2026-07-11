3e6e04ce25efe5b91a0abc88e1c9996adea19b65
CHimPat073 <himanshupathak073@gmail.com>
2026-07-12 00:48:06 +0530

    Add Supreme/Stack & Queue/Stack: MinimumAddToValid

---
+++ Supreme/Stack & Queue/Stack/MinimumAddToValid.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int minAddToMakeValid(string s) {
+        stack<int> st;
+        int cnt=0;
+        int closing = 0;
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+            if(ch=='('){
+                st.push(ch);
+            }else{
+                if(st.empty()){
+                    closing++;
+                }else{
+                    st.pop();
+                }
+            }
+
+        }
+        return st.size() + closing;
+    }
+int main(){
+    string s="()))((";
+    cout<<minAddToMakeValid(s);
+    
+    return 0;
+}
