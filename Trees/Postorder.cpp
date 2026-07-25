a34977366256c63d265ac6973f0a37cb246dd343
CHimPat073 <himanshupathak073@gmail.com>
2026-07-26 01:06:17 +0530

    Update Trees: Postorder

--- Trees/Postorder.cpp
+++ Trees/Postorder.cpp
@@ -44,6 +44,34 @@ vector<int> postorderTraversalitr(Node* root) {
         }
         return ans;
     }
+vector<int> postorderTraversalitr2(Node*root){
+        Node* curr=root;
+        vector<int> ans;
+        stack<Node*> st;
+        while(curr!=NULL || !st.empty()){
+            if(curr!=NULL){
+                st.push(curr);
+                curr=curr->left;
+            }
+            else{
+                Node* temp= st.top()->right;
+                if(temp==NULL){
+                    temp=st.top();
+                    st.pop();
+                    ans.push_back(temp->data);
+                    while(!st.empty() && temp==st.top()->right){
+                        temp=st.top();
+                        st.pop();
+                        ans.push_back(temp->data);
+                    }
+                }
+                else{
+                    curr=temp;
+                }
+            }
+        }
+        return ans;
+    }
 int main(){
     struct Node* root=new Node(1);
     root->left=new Node(2);
