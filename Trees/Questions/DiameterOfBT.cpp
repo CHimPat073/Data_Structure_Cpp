f9a664330d73af07357a387e2b75acaca79bd6fe
CHimPat073 <himanshupathak073@gmail.com>
2026-07-27 23:46:42 +0530

    Update Trees/Questions: DiameterOfBT

--- Trees/Questions/DiameterOfBT.cpp
+++ Trees/Questions/DiameterOfBT.cpp
@@ -26,6 +26,22 @@ int maxi=0;
 
         return maxi;
     }
+    int helper2(TreeNode* root,int &dia){
+        if(root==NULL) return 0;
+
+        int lh=helper2(root->left,dia);
+        int rh=helper2(root->right,dia);
+
+        dia=max(dia,lh+rh);
+
+        return 1+max(lh,rh);
+    }
+    int diameterOfBinaryTree2(TreeNode* root) {
+        int dia=0;
+        helper2(root,dia);
+        return dia;
+
+    }
 int main(){
     struct TreeNode* root=new TreeNode(1);
     root->left=new TreeNode(2);
@@ -34,7 +50,7 @@ int main(){
     root->left->left=new TreeNode(4);
     root->left->right=new TreeNode(5);
 
-    bool ans=diameterOfBinaryTree(root);
+    bool ans=diameterOfBinaryTree2(root);
     cout<<ans;
     return 0;
 }
