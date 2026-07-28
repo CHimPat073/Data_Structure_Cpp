41aadb3aa9d9fb2b2922942a3904b87a1eb4f0dd
CHimPat073 <himanshupathak073@gmail.com>
2026-07-29 00:52:43 +0530

    Add Trees/Questions: ZigZagtraversal

---
+++ Trees/Questions/ZigZagtraversal.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode{
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+
+    TreeNode(int val){
+        this->val=val;
+        left=right=NULL;
+    }
+};
+ vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==NULL) return ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> level;
+            for(int i=0;i<size;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                if(node->right!=NULL) q.push(node->right);
+                if(node->left!=NULL) q.push(node->left);
+                level.push_back(node->val);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+int main(){
+    struct TreeNode* root=new TreeNode(1);
+    root->left=new TreeNode(2);
+    root->right=new TreeNode(3);
+    
+    root->left->left=new TreeNode(4);
+    root->left->right=new TreeNode(5);
+
+    vector<vector<int>> ans = zigzagLevelOrder(root);
+    for(int i=0;i<ans.size();i++){
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
